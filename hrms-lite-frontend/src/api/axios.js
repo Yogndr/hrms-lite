ca488ad5a21cda68461baeb2de6a9a4e41f8cb59
Yogender <yashmittal92002@gmail.com>
2026-02-18 19:06:21 +0530

    updated

--- hrms-lite-frontend/src/api/axios.js
+++ hrms-lite-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000", // change to live URL later
+  baseURL: "https://hrms-lite-q3re.onrender.com", // change to live URL later
 });
 
 export default API;
